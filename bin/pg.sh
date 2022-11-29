3ad7058cea016f4efd9432954d8f235894b6c5fe
d3v-zero <d3uter@proton.me>
2022-11-29 12:31:33 +0100

    2022-11-29-12:31:33

--- bin/pg.sh
+++ bin/pg.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]; then
     read -p "Podaj nazwę szukanego procesu: "
-    ps ax | grep -i "$REPLY" | grep -v pg.sh
+    ps ax | grep -i "$REPLY" | grep -v "grep -i $REPLY"
 else
-    ps ax | grep -i "$1" | grep -v pg.sh
+    ps ax | grep -i "$1" | grep -v "pg.sh" | grep -v "grep -i $1"
 fi
